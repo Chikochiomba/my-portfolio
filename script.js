c9b4c3677cc256664ed9266a1b275a3c5e3a8851
chikochiomba <chikochiomba@gmai.com>
2023-06-01 18:16:30 +0200

    updates

---
+++ script.js
@@ -0,0 +1,26 @@
+/*this is a greeting function to generate dynamic greetings based on time*/
+let container = document.querySelector(".greeting-container");
+let timeNow = new Date().getHours();
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good evening";
+container.innerHTML = `<h4>${greeting}</h4>`;
+/*form validation function in javascript*/
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
